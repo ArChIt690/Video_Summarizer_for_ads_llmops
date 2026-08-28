3b63eae5e1c23470a67b6b27886412bf0b50ec2b
Archit chakraborty <architchakraborty47@gmail.com>
2026-08-29 03:34:43 +0530

    audit content node created

--- Backend/src/graph/nodes.py
+++ Backend/src/graph/nodes.py
@@ -33,7 +33,7 @@ def Video_indexer_node(state : VideoAudit) -> Dict[str , Any]:
     try:
         #video downloader and storing into a temporary path
         if "youtube.com" in video_url_input or "youtube.be" in video_url_input:
-            logging.INFO{"video downloading ....."}
+            logging.INFO("video downloading .....")
             video_download = vi_service.download_video(video_url_input , output_path = local_path)
         else:
             raise Exception("pls provide a correct url")
@@ -89,4 +89,67 @@ def audit_content_node(state : VideoAudit)-> Dict[str , Any]:
         AZURE_SEARCH_API_KEY = os.getenv("AZURE_SEARCH_API_KEY"),
     )
 
-    
+    #taking the ocr text and transcript and doing similiarity searching on the vector database 
+    ocr_text = state.get("ocr_text", [])
+    transcript = state.get("transcript")
+    query_text = f"{transcript} {'\n'.join(ocr_text)}"
+    docs = AzureSearch.similarity_search(query_text , k=3)
+
+    retreive_rules = "\n\n".join(docs.page_content() for doc in docs)
+
+    SystemMessage=f"""
+                You are an Video Auditer who's job is to judge the video ad
+                You will take the relevant information related to the video and the input from
+                 {retreive_rules}
+                  
+                This are the set of Instructions You will follow :
+                1. Analyze the Transcript and the OCR text
+                2. Identify any violations in the video
+                3. You will strictly return in a JSON format, the format i am giving below:
+
+                {{
+                        "compliance_results": [
+                            {{
+                                "category": "Claim Validation",
+                                "severity": "CRITICAL",
+                                "description": "Explanation of the violation..."
+                            }}
+                        ],
+                        "status": "FAIL", 
+                        "final_report": "Summary of findings..."
+                    }}
+                If no Violations is found set the final_status to "PASS" and compliance_result = []
+                    
+                 """
+
+    user_message = f"""
+                VIDEO METADATA : {state.get("metadata , []")},
+                Transcript : {transcript},
+                ON SCREEN TEXT (OCR) : {ocr_text}
+                """
+
+    try:
+        response = llm.invoke(SystemMessage = SystemMessage,
+                   HumanMessage = user_message)
+        # --- FIX: Clean Markdown if present (```json ... ```) ---
+        content = response.content
+        if "```" in content:
+            # Regex to find JSON inside code blocks
+            content = re.search(r"```(?:json)?(.*?)```", content, re.DOTALL).group(1)
+        
+        audit_data = json.loads(content.strip())
+
+        return{
+            "compliance_result" : audit_data.get("compliance_result" , []),
+            "final_status" : audit_data.get("final_status" , "FAIL"),
+            "final_report" : audit_data.get("final_report" , "")
+
+        }
+    except Exception as e:
+        logger.error(f"Something went wrong in invoking the llm : {str(e)} ")
+
+        return{
+            "final_status" : "FAIL",
+            "final_report" : "llm invoking failed check first so no audit report",
+        }
+                
