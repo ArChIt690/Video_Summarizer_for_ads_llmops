54dd1e4c88bf6f0843a3e88e946d40b2541efed3
Archit chakraborty <architchakraborty47@gmail.com>
2026-09-03 00:29:15 +0530

    final main.py

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from video-summarizer-for-ads-llmops!")
+from pprint import pprint
+import logging
+import  uuid
+import json
+from Backend.src.graph.workflow import Workflow
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                    )
+
+logger = logging.getLogger("final_main")
+
+def create_main():
+    session_id = str(uuid.uuid4())
+    logging.info(f"Starting the session with session_id: {session_id}")
+
+    initial_input = {
+        "video_url": "https://www.youtube.com/watch?v=example_video_id",
+
+        "video_id": f"vid_{session_id[:8]}",
+
+        "compliance_results": [],
+    }
+
+    print(f"input payload : {json.dumps(initial_input , indent = 4)}")
+
+    try:
+        workflow = Workflow.invoke(initial_input)
+
+        print(f"Video Id : {workflow.get('video_id')}")
+        print(f"STATUS : {workflow.get('status')}")
+        results = workflow.get('compliance_results', [])
+
+        if results:
+            for issues in results:
+                print(f" = [{issues.get('severity')}] , {issues.get('category')} , {issues.get('description')}")
+
+        else:
+            print("No compliance issues found.")
+
+        print(f"Final output \n {workflow.get('final_report')}")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    create_main()
